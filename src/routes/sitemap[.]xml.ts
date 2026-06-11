307644438013b32f06598ee5f1878fdbfdefeb6a
tijcef <tijwuncare@gmail.com>
2026-06-11 12:42:54 +0100

    Fix sitemap generation

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,13 +1,26 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+const WP_API = "https://cms.tijcef.org/wp-json/wp/v2";
+
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
     handlers: {
       GET: async () => {
+        const postsRes = await fetch(
+          `${WP_API}/posts?per_page=100`
+        );
+
+        const posts = await postsRes.json();
+
         const urls = [
           "https://clearfact.ng/",
           "https://clearfact.ng/about",
           "https://clearfact.ng/contact",
+
+          ...posts.map(
+            (post: any) =>
+              `https://clearfact.ng/post/${post.slug}`
+          ),
         ];
 
         const xml = `<?xml version="1.0" encoding="UTF-8"?>
